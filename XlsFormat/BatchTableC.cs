236e5658e80ef7b9d78c70d6a4ba3e937296fe77
Unknown <winpooh32@yandex.ru>
2017-11-30 16:29:28 +1000

    Initial implementation of BatchTableC class

---
+++ XlsFormat/BatchTableC.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using ClosedXML.Excel;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace XlsFormat
+{
+    public class BatchTableC
+    {
+        public struct ColumnNames{
+            public string number;
+            public string allPlaces;
+            public string placesByType;
+            public string name;
+            public string price;
+
+            public string bagNumber;
+            public string bagWeight;
+        }
+
+        public struct Product  
+        {  
+            //номер у нас будет ключем в словаре
+            public UInt32 allPlaces; //2. Суммарное количество вложений (количество мест в пакете)
+            public UInt32 placesByType; // 3. Количество объектов конкретного типа в посылке
+            public string name;
+            public decimal price;
+
+            //мешки
+            public UInt64 bagNumber;
+            public Decimal bagWeight;
+        }  
+
+        public double weightNet;//нетто
+        public double weightPackage;//вес упаковки
+        public double weightGross;//брутто
+
+        public Dictionary<string, List<Product>> goods = new Dictionary<string, List<Product>>(500);
+
+        private Regex priceRegex = new Regex(@"^[\,\.\d+]*");
+
+        public BatchTableC (string file)
+        {
+            try{
+                var workbook = new XLWorkbook(file);
+
+                ColumnNames columnsMap = new ColumnNames{
+                    number = "A",
+                    allPlaces = "T",
+                    placesByType ="AG",//FIXME возможно надо поменять местами
+                    name = "AK",
+                    price = "AO",
+
+                    bagNumber = "B",
+                    bagWeight = "C"
+                };
+
+                loadGoods(workbook.Worksheet(1), columnsMap);//Лист “Товары”
+
+            }
+            catch(Exception ex){
+                Console.WriteLine(ex);
+                throw new ArgumentException("[BatchTableC] Error in file: " + file);
+            }
+        }
+
+        private void loadGoods(IXLWorksheet worksheet, ColumnNames columnsMap){
+            var numberColumn        = worksheet.Columns(columnsMap.number);
+            var allPlacesColumn     = worksheet.Columns(columnsMap.allPlaces);
+            var placesByTypeColumn  = worksheet.Columns(columnsMap.placesByType);
+            var nameColumn          = worksheet.Columns(columnsMap.name);
+            var price               = worksheet.Columns(columnsMap.price);
+
+            var cellsNumberColumn        = numberColumn.CellsUsed();
+            var cellsAllPlacesColumn     = allPlacesColumn.CellsUsed();
+            var cellsPlacesByTypeColumn  = placesByTypeColumn.CellsUsed();
+            var cellsNameColumn          = nameColumn.CellsUsed();
+            var cellsPrice               = price.CellsUsed();
+
+            var enumerNumberColumn        = cellsNumberColumn.GetEnumerator();
+            var enumerAllPlacesColumn     = cellsAllPlacesColumn.GetEnumerator();
+            var enumerPlacesByTypeColumn  = cellsPlacesByTypeColumn.GetEnumerator();
+            var enumerNameColumn          = cellsNameColumn.GetEnumerator();
+            var enumerPrice               = cellsPrice.GetEnumerator();
+
+            string key;
+            Product val;
+
+            //Пропускаем заголовки
+            enumerNumberColumn.MoveNext();
+            enumerAllPlacesColumn.MoveNext();
+            enumerPlacesByTypeColumn.MoveNext();
+            enumerNameColumn.MoveNext();
+            enumerPrice.MoveNext();
+
+            int i = 0;
+
+            while(
+                enumerNumberColumn.MoveNext()       && 
+                enumerAllPlacesColumn.MoveNext()    && 
+                enumerPlacesByTypeColumn.MoveNext() && 
+                enumerNameColumn.MoveNext()         && 
+                enumerPrice.MoveNext()
+            ){
+                ++i;
+
+                try{
+                    key = enumerNumberColumn.Current.GetValue<string>().Trim();
+                    val = new Product { 
+                        allPlaces       = Convert.ToUInt32(enumerAllPlacesColumn.Current.GetValue<string>().Trim()),
+                        placesByType    = Convert.ToUInt32(enumerPlacesByTypeColumn.Current.GetValue<string>().Trim()),
+                        name            = enumerNameColumn.Current.GetValue<string>().Trim(),
+                        price           = Convert.ToDecimal( normalizePrice(enumerPrice.Current.GetValue<string>().Trim()) )
+                    };
+
+                    List<Product> values;
+
+                    if (!goods.TryGetValue(key, out values)) {
+                        values = new List<Product>();
+                        goods.Add(key, values);
+                    }
+
+                    values.Add(val);
+                }
+                catch(Exception ex){
+                    //игнорируем повторения ключа
+                    //TODO уведомление
+
+                    Console.WriteLine (i);
+                    Console.WriteLine (ex);
+                }
+            }
+        }
+
+        private void loadBags(IXLWorksheet worksheet){
+
+        }
+
+        private string normalizePrice(string rawPrice){
+            Match match = priceRegex.Match(rawPrice);
+
+            if (match.Success) {
+                //для формата decimal
+                //https://msdn.microsoft.com/ru-ru/library/cafs243z(v=vs.110).aspx
+                return match.Value.Replace (',', '.');
+            } else {
+                throw new ArgumentException("[BatchTableC] Broken price: " + rawPrice);
+            }
+        }
+    }
+}
+
